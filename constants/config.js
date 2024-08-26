6ab8dbc4af8e6cdcfc9a05b9c04dffc5569d74b1
Victry0099 <prakashkush0099@gmail.com>
2024-08-26 15:13:46 +0530

    modified:   constants/config.js

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,10 @@
 const corsOption = {
-  origin: ["http://localhost:5173", process.env.CLIENT_URL],
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL,
+    "http://mtaindia.org",
+    "http://www.mtaindia.org",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
