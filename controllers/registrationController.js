4dabf7c99130afbe369069c9659185ab4077d798
Victry0099 <prakashkush0099@gmail.com>
2024-09-18 11:34:16 +0530

    modified:   .env 	modified:   controllers/registrationController.js

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,64 +1,3 @@
-// import Registration from "../models/registration.js";
-// import nodemailer from "nodemailer";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// export const createRegistration = async (req, res) => {
-//   const { name, email, number, courses, trainingMode } = req.body;
-
-//   try {
-//     // Save the registration to the database
-//     const newRegistration = new Registration({
-//       name,
-//       email,
-//       number,
-//       courses,
-//       trainingMode,
-//     });
-
-//     await newRegistration.save();
-
-//     // Create a transporter object using the default SMTP transport
-//     const transporter = nodemailer.createTransport({
-//       service: "gmail", // You can use your email service provider
-//       port: 465,
-//       auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS,
-//       },
-//     });
-
-//     // Set up email data
-//     const mailOptions = {
-//       from: process.env.EMAIL_USER,
-//       to: process.env.RECIPIENT_EMAIL,
-//       subject: "New Registration Received",
-//       text: `New Registration Details:
-//       Name: ${name}
-//       Email: ${email}
-//       Phone Number: ${number}
-//       Courses: ${courses}
-//       Training Mode: ${trainingMode}`,
-//     };
-
-//     // Send mail with defined transport object
-//     transporter.sendMail(mailOptions, (error, info) => {
-//       if (error) {
-//         console.error("Failed to send email:", error);
-//         return res.status(500).json({ message: "Failed to send email", error });
-//       }
-//       console.log("Email sent successfully:", info.response);
-//       res
-//         .status(201)
-//         .json({ message: "Registration saved and email sent successfully" });
-//     });
-//   } catch (error) {
-//     console.error("Error saving registration:", error);
-//     res.status(500).json({ message: "Failed to save registration", error });
-//   }
-// };
-
 import Registration from "../models/registration.js";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
@@ -93,7 +32,7 @@ export const createRegistration = async (req, res) => {
     // Email to the recipient (admin)
     const mailOptionsAdmin = {
       from: process.env.EMAIL_USER,
-      to: process.env.RECIPIENT_EMAIL,
+      to: `${process.env.RECIPIENT_EMAIL}, anilk.mtaindia@gmail.com`,
       subject: "New Registration Received",
       text: `New Registration Details:
       Name: ${name}
@@ -118,7 +57,7 @@ Course Details:\n
 
 Our team will be reaching out to you within the next 24 hours with all the necessary details and next steps.\n
 
-If you have any immediate questions, feel free to  contact us at 9984797584.\n
+If you have any immediate questions, feel free to  contact us at 9315636357.\n
 
 Welcome aboard, and we look forward to helping you achieve your IT goals!\n
 
